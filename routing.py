1de64737930bd9d618ce5d6f813b192e135bd47d
J.Valkonen <johannes.valkonen@helsinki.fi>
2021-04-25 21:28:42 +0300

    better validation for register form

--- routing.py
+++ routing.py
@@ -2,15 +2,9 @@ from app import app
 import memes
 import users
 import re
+import imghdr
 from flask import (render_template, make_response, request, redirect)
-from flask.ext.navigation import Navigation
 from random import randint
-
-app.Bar('navbar', [
-    app.Item('Memelation', 'index'),
-    app.Item('Users', 'users'),
-    app.Item('Random', 'random'),
-])
 
 @app.route('/')
 def index():
@@ -49,6 +43,7 @@ def send():
     imgupload = request.files["file"]
     img_filename = imgupload.filename
     img_data = imgupload.read()
+    print(imghdr.what(img_data))
     if not img_filename.endswith((".jpg", ".jpeg", ".png", ".gif")): 
         return "JPG, PNG or GIF files only plz."
     if len(img_data) > 1024 * 1024: return "Your meme is too heavy! Whydontcha downsize it a bit (or two)?"
